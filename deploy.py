226e1a9a9ffaabf8c9ceefe47c3427968529f4de
Ankur Saxena <flyankur@gmail.com>
2014-03-06 20:27:07 +0530

    Testing Hachiko

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 '''
 Hachiko - A very simple continous integration app built on Flask
-Fork at https://github.com/flyankur/hachiko
+Fork Hachiko at https://github.com/flyankur/hachiko
 '''
 
 from raven.contrib.flask import Sentry
